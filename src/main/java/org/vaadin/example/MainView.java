af0caf7f0f9434e03c341661a41871f4c3548828
Aliaksandr Liakh <aaa@epam.com>
2024-03-11 16:18:36 +0300

    Create

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -6,17 +6,16 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabSheet;
-import com.vaadin.flow.component.tabs.TabsVariant;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
 public class MainView extends VerticalLayout {
 
     public MainView() {
+        Dialog dialog = new Dialog();
+        dialog.setHeaderTitle("Workflow");
+
         TabSheet tabSheet = new TabSheet();
         tabSheet.setSizeFull();
 
@@ -33,44 +32,41 @@ public class MainView extends VerticalLayout {
         Button nextButton = new Button("Next >");
 
         prevButton.addClickListener(event -> {
-            System.out.println("prev button " + tabSheet.getSelectedIndex());
             if (tabSheet.getSelectedIndex() > 0) {
                 tabSheet.setSelectedIndex(tabSheet.getSelectedIndex() - 1);
             }
-            prevButton.setEnabled(tabSheet.getSelectedIndex() > 0);
-            nextButton.setEnabled(tabSheet.getSelectedIndex() < 2);
+            buttonsVisibility(tabSheet, prevButton, nextButton);
         });
         nextButton.addClickListener(event -> {
-            System.out.println("next button " + tabSheet.getSelectedIndex());
             if (tabSheet.getSelectedIndex() < 2) {
                 tabSheet.setSelectedIndex(tabSheet.getSelectedIndex() + 1);
             }
-            prevButton.setEnabled(tabSheet.getSelectedIndex() > 0);
-            nextButton.setEnabled(tabSheet.getSelectedIndex() < 2);
+            buttonsVisibility(tabSheet, prevButton, nextButton);
         });
         Button close = new Button("Close");
-
-        HorizontalLayout buttons = new HorizontalLayout(prevButton, nextButton, close);
-        buttons.setHeight("50px");
-
-        VerticalLayout content = new VerticalLayout(tabSheet, buttons);
-
-        Dialog dialog = new Dialog();
-        dialog.setHeaderTitle("Workflow");
-        dialog.add(content);
-
-        Button start = new Button("show dialog");
-        start.addClickListener(event -> {
-            tabSheet.setSelectedIndex(0);
-            prevButton.setEnabled(false);
-            nextButton.setEnabled(true);
-
-            dialog.open();
-        });
         close.addClickListener(event -> {
             dialog.close();
         });
 
-        add(start);
+        HorizontalLayout buttonsLayout = new HorizontalLayout(prevButton, nextButton, close);
+        buttonsLayout.setHeight("50px");
+
+        VerticalLayout contentLayout = new VerticalLayout(tabSheet, buttonsLayout);
+        dialog.add(contentLayout);
+
+        Button showButton = new Button("show dialog");
+        showButton.addClickListener(event -> {
+            tabSheet.setSelectedIndex(0);
+            buttonsVisibility(tabSheet, prevButton, nextButton);
+
+            dialog.open();
+        });
+
+        add(showButton);
+    }
+
+    private void buttonsVisibility(TabSheet tabSheet, Button prevButton, Button nextButton) {
+        prevButton.setEnabled(tabSheet.getSelectedIndex() > 0);
+        nextButton.setEnabled(tabSheet.getSelectedIndex() < 2);
     }
 }
